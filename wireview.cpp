9dbe9f11933012c733fa2289e166ed97215baf69
Sidney <segoldinger@wpi.edu>
2022-11-11 14:52:19 -0500

    edited cpp

--- wireview.cpp
+++ wireview.cpp
@@ -1,8 +1,15 @@
 //
 // Created by Sidney Goldinger on 11/11/22.
 //
+#include <stdio.h>
+#include <stdlib.h>
+#include <pcap.h>  /* GIMME a libpcap plz! */
+#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-int main () {
+int main (int argc, char **argv) {
     // open the input file
 
     // loop through the input file
