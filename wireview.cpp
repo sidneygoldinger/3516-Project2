f3fb5a2541e1a1730958b99ceb30ab634a52b240
Connor <codingconrad49@gmail.com>
2022-11-19 13:07:28 -0500

    added printing of UDP source and destination port numbers

--- wireview.cpp
+++ wireview.cpp
@@ -12,6 +12,7 @@
 #include <arpa/inet.h>
 #include <net/ethernet.h>
 #include <netinet/ip.h>
+#include <netinet/udp.h>
 /////// Global variables ///////
 int totalNumberPackets = 0;
 
@@ -39,18 +40,57 @@ void callback(u_char *thing1, const struct pcap_pkthdr *thing2, const u_char *th
     //https://www.iana.org/assignments/ieee-802-numbers/ieee-802-numbers.xhtml
     printf("IP or ARP: %d\n", ntohs(e_header->ether_type));
     
-    //u_int8_t hostDestinationAddr[ETH_ALEN];
     printf("ethernet header source: %s\n", ether_ntoa((const struct ether_addr *)&e_header->ether_shost));
     printf(" destination: %s \n", ether_ntoa((const struct ether_addr *)&e_header->ether_dhost));
 
 
     thing3 = thing3 + sizeof(*e_header);
     printf("%ld\n", sizeof(*e_header));
+
+    //     struct ip {
+    // #if BYTE_ORDER == LITTLE_ENDIAN 
+    //     u_char  ip_hl:4,        /* header length */
+    //         ip_v:4;         /* version */
+    // #endif
+    // #if BYTE_ORDER == BIG_ENDIAN 
+    //     u_char  ip_v:4,         /* version */
+    //         ip_hl:4;        /* header length */
+    // #endif
+    //     u_char  ip_tos;         /* type of service */
+    //     short   ip_len;         /* total length */
+    //     u_short ip_id;          /* identification */
+    //     short   ip_off;         /* fragment offset field */
+    // #define IP_DF 0x4000            /* dont fragment flag */
+    // #define IP_MF 0x2000            /* more fragments flag */
+    //     u_char  ip_ttl;         /* time to live */
+    //     u_char  ip_p;           /* protocol */
+    //     u_short ip_sum;         /* checksum */
+    //     struct  in_addr ip_src,ip_dst;  /* source and dest address */
+    // };
+
+
     struct ip* ip_header = ((struct ip*) thing3);
     printf("ip header source: %s\n", inet_ntoa((struct in_addr)ip_header->ip_src));
     printf("ip header destination: %s\n", inet_ntoa((struct in_addr)ip_header->ip_dst));
+    u_char upperProtocolNum = ip_header->ip_p;
+    printf("UDP or not (should be 17 for UDP): %d\n", upperProtocolNum);
 
-    thing3 = thing3 + sizeof(ip_header);
+    //if UDP is carried
+    //struct udphdr
+    // {
+    // u_int16_t uh_sport;                /* source port */
+    // u_int16_t uh_dport;                /* destination port */
+    // u_int16_t uh_ulen;                /* udp length */
+    // u_int16_t uh_sum;                /* udp checksum */
+    // };
+    if(upperProtocolNum == 17) {
+        thing3 = thing3 + sizeof(*ip_header);
+        struct udphdr* udp_header = ((struct udphdr*) thing3);
+        printf("udp header source port: %d\n", ntohs(udp_header->uh_sport));
+        printf("udp header destination port: %d\n", ntohs(udp_header->uh_dport));
+    }
+
+
     // for(int i = 0; i < ETH_ALEN; i ++) {
     //     hostDestinationAddr[i] = ntohs(e_header->ether_dhost[i]);
     //     printf("%d ", ntohs(e_header->ether_dhost[i]));
